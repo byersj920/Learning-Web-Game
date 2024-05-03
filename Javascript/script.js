63cec6f74270a59862b4e8d213320c172f948c52
Jonathan <byersj920@gmail.com>
2024-05-02 19:47:46 -0500

    Basic code started

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript code for the game will go here
+const rockButton = document.getElementById('rockButton');
+const goldCountElement = document.getElementById('goldCount');
+
+let goldCount = 0;
+let earnedGold = 0;
+
+const generateGold = () =>{
+    earnedGold = Math.ceil(Math.random() * 20);
+    goldCount += earnedGold;
+    goldCountElement.textContent = goldCount;
+}
+
+rockButton.addEventListener('click', function() {
+    generateGold();
+    console.log("Wow! You mined " + earnedGold + " gold. Your current gold count is: " + goldCount);
 });
